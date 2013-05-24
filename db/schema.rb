d70af84e5755be7f488204f7da6413e9db47630a
Rainer Weitz <rweitz@fulcrum.net>
2013-05-24 07:34:59 -0700

    Fixes to the Part Detail forms.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523004958) do
+ActiveRecord::Schema.define(:version => 20130523191210) do
 
   create_table "entries", :force => true do |t|
     t.string   "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20130523004958) do
     t.string   "mfg_part_code"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "manufacturer"
   end
 
   create_table "people", :force => true do |t|
